9b9cce224bab08048f6682bf0aa04362996b053d
Britany Villalobos <britanyvsalazar@gmail.com>
2026-05-21 15:03:10 -0600

    Arreglo en la memoria

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,10 +2,12 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
-defineBackend({
+export const backend = defineBackend({
   auth,
   data,
 });
+
+// Force smaller memory for bucket deployment lambdas
+const stack = backend.createStack('custom-stack');
+
+stack.templateOptions.description = 'Custom stack to reduce Lambda memory';
